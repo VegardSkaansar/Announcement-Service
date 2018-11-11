f3866fd0c86459584fbc9c614711a38b01b7446c
VegardSkaansar <vegaskaa@stud.ntnu.no>
2018-11-11 18:18:39 +0100

    added the routing of the server and initialized main

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"goprojects/AnnonceService/webserver"
+	"net/http"
+	"os"
+)
+
+// This is the main and are supposed to delegate the work to the other packages
+// This can be comparable to a controller in model-view-controller. We can think
+// of the packages as the model and html packes as the view where the customers
+// can get the information they need
+
+// main is in control of starting the server, assign a port and ipadress to the server
+// main will listen to the ipadress/port for request and send it to the right
+// handler
+func main() {
+
+	port := os.Getenv("PORT")
+
+	http.HandleFunc("/Announce", webserver.Routing())
+	http.ListenAndServe(":"+port, nil)
+}
