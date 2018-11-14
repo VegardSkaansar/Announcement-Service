6fc8b8744b68cc5bc10dca1c0dcc4e2e2d647969
VegardSkaansar <vegaskaa@stud.ntnu.no>
2018-11-14 22:36:31 +0100

    Made a token creater and a decoder, and will make us the jwt tokens

---
+++ webserver/jwt.go
@@ -0,0 +1,41 @@
+package webserver
+
+import (
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// secret signing value are made here, security reasons should this have been a value that no one could see
+// but because of this project we do it very simple and make it here
+var hmacSecret = []byte("Secret")
+
+// createToken takes a username and a password and uses hmac encoding and encodes it with the secret word
+func createToken(username string, cryptPass string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": username,
+		"password": cryptPass,
+		"exp":      0,
+	})
+
+	tokenString, err := token.SignedString(hmacSecret)
+	return tokenString, err
+}
+
+// decodeToken takes a token from the header of a client and decodes it
+func decodeToken(token string) (interface{}, interface{}) {
+	data, err := jwt.Parse(token, func(tokenString *jwt.Token) (interface{}, error) {
+
+		if _, ok := tokenString.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", tokenString.Header["alg"])
+		}
+		return hmacSecret, nil
+	})
+
+	if claims, ok := data.Claims.(jwt.MapClaims); ok && data.Valid {
+		return claims["username"], claims["password"]
+	} else {
+		return false, false
+	}
+
+}
