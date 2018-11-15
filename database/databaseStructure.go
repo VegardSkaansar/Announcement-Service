5fe9c63448de1eb59dc7c49c3912635da8453b6e
VegardSkaansar <vegaskaa@stud.ntnu.no>
2018-11-15 11:39:38 +0100

    changing the datastructure to better fit our mongodb structure

--- database/databaseStructure.go
+++ database/databaseStructure.go
@@ -6,6 +6,7 @@ import "gopkg.in/mgo.v2/bson"
 // a non sql database connection
 type MongoDB struct {
 	DatabaseURL      string
+	DatabaseName     string
 	DatabaseUser     string
 	DatabaseAnnounce string
 }
@@ -21,6 +22,7 @@ type User struct {
 	LastName  string        `json:"lastname"`
 	Year      string        `json:"year"`
 	Role      string        `json:"-" bson:"role"`
+	anno      Announce
 }
 
 // Announce is the db structure for all the announcement given from
@@ -29,6 +31,55 @@ type Announce struct {
 	Title       string        `json:"title" bson:"title"`
 	Description string        `json:"description" bson:"description"`
 	ObjectID    bson.ObjectId `json:"-" bson:"_id"`
-	UserLink    bson.ObjectId `json:"-" bson:"user"`
 	Cost        int           `json:"cost"`
+}
+
+// StorageUser interface for the database for adding users and deleting
+// Only admins can use this interface
+type StorageUser interface {
+	AddUser(person User)
+	DeleteUser(username string)
+	ExistUser(username string) bool
+}
+
+// AddUser adds a user to the datastructure
+func (db *MongoDB) AddUser(person User) {
+
+}
+
+// DeleteUser deletes a user from the db
+func (db *MongoDB) DeleteUser(username string) {
+
+}
+
+// ExistUser checks if a user with this username exist
+func (db *MongoDB) ExistUser(username string) bool {
+
+	return true
+}
+
+// StorageAnnouncement non admin inteface and here can clients
+// add their Annoucements delete or modify
+type StorageAnnouncement interface {
+	AddAnnouncement(ad Announce) bool
+	DeleteAnnouncement(title string) bool
+	ModifyAnnouncement(title string) bool
+}
+
+// AddAnnouncement adds a users announce
+func (db *MongoDB) AddAnnouncement(ad Announce) bool {
+
+	return true
+}
+
+// DeleteAnnouncement takes a announce with a title and deletes it
+func (db *MongoDB) DeleteAnnouncement(title string) bool {
+
+	return true
+}
+
+// ModifyAnnouncement Takes a title and modifies what information the user wants
+func (db *MongoDB) ModifyAnnouncement(title string) bool {
+
+	return true
 }
