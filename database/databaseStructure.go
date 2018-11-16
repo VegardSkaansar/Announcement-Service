8a074d71257fc1c40220c66203793044395d34e8
VegardSkaansar <vegaskaa@stud.ntnu.no>
2018-11-16 21:28:26 +0100

    modifying structure to fit my db

--- database/databaseStructure.go
+++ database/databaseStructure.go
@@ -14,13 +14,12 @@ type MongoDB struct {
 // Role and password will be stored in db but no other way to get
 // this information from the db
 type User struct {
-	ObjectID  bson.ObjectId `json:"-" bson:"_id"`
-	Username  string        `json:"username" bson:"username"`
-	Password  string        `json:"-" bson:"password"`
-	FirstName string        `json:"firstname"`
-	LastName  string        `json:"lastname"`
-	Year      string        `json:"year"`
-	Admin     bool          `json:"-" bson:"admin"`
+	Username  string `json:"username" bson:"username"`
+	Password  string `json:"-" bson:"password"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Year      string `json:"year"`
+	Admin     bool   `json:"-" bson:"admin"`
 }
 
 // Announce is the db structure for all the announcement given from
@@ -36,6 +35,7 @@ type Announce struct {
 // so we will have a user with its information and then
 // have all of the announces this user have posted
 type Collection struct {
-	person User
-	ads    []Announce
+	ObjectID bson.ObjectId `json:"-" bson:"_id"`
+	person   User
+	ads      []Announce
 }
